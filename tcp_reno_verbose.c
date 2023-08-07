406db43200a90f56961be883bf48f932acacf8ee
janev94 <misho.janew@gmail.com>
2023-08-07 12:10:45 +0000

    Added an option to specify initial ssthresh exit point

--- tcp_reno_verbose.c
+++ tcp_reno_verbose.c
@@ -3,6 +3,10 @@
 #include <linux/vmalloc.h>
 
 
+static int initial_ssthresh __read_mostly;
+module_param(initial_ssthresh, int, 0644);
+MODULE_PARM_DESC(initial_ssthresh, "initial value of slow start threshold");
+
 struct vrenotcp {
 	u32 saved_reset_cnt;
 
@@ -18,8 +22,8 @@ void tcp_vreno_in_ack_event(struct sock *sk, u32 flags)
 	uint16_t dport = ntohs(isock->inet_dport);
 
 	if(sport == 80) { // HTTP server doing
-		printk(KERN_INFO "ACK Received. sourcep: %u dstp: %u proto%u send window: %u recv window: %u ssthresh: %u\n",
-				sport, dport, sk->sk_protocol, tp->snd_cwnd, tp->rcv_wnd, tp->snd_ssthresh);
+		printk(KERN_INFO "ACK Received. sourcep: %u dstp: %u proto%u send window: %u recv window: %u ssthresh: %u slow-start: %u\n",
+				sport, dport, sk->sk_protocol, tp->snd_cwnd, tp->rcv_wnd, tp->snd_ssthresh, tp->snd_cwnd < tp->snd_ssthresh);
 	}
 }
 
@@ -27,7 +31,7 @@ void tcp_vreno_in_ack_event(struct sock *sk, u32 flags)
 static inline void vreno_reset(struct vrenotcp *ca)
 {
 
-        ca->saved_reset_cnt = 0;
+		ca->saved_reset_cnt = 0;
 }
 
 
@@ -35,6 +39,13 @@ void tcp_vreno_init(struct sock *sk)
 {
 
 	struct vrenotcp *ca = inet_csk_ca(sk);
+
+
+	if(initial_ssthresh) 
+	{
+		printk(KERN_INFO "INITIAL SSTHRESH: %u", initial_ssthresh);
+		tcp_sk(sk)->snd_ssthresh = initial_ssthresh;
+	}
 
 	vreno_reset(ca);
 }
@@ -92,15 +103,15 @@ struct tcp_congestion_ops tcp_reno_verbose = {
 
 static int __init tcp_reno_verbose_register(void)
 {
-    printk(KERN_INFO "Verbose Reno Going Up5");
+	printk(KERN_INFO "Verbose Reno Going Up5");
 	return tcp_register_congestion_control(&tcp_reno_verbose);
 }
 
 
 static void __exit tcp_reno_verbose_unregister(void)
 {
-    printk(KERN_INFO "Verbose Reno Shutting Down5");
-    tcp_unregister_congestion_control(&tcp_reno_verbose);
+	printk(KERN_INFO "Verbose Reno Shutting Down5");
+	tcp_unregister_congestion_control(&tcp_reno_verbose);
 }
 
 
